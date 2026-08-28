4a608e4d33ec022197a9225c987e6157381dcc35
Juan Sebastian Echeverri Gallego <hornetf256@gmail.com>
2026-08-28 18:58:09 -0500

    feat: extend status endpoint

--- Controllers/StatusController.cs
+++ Controllers/StatusController.cs
@@ -12,4 +12,9 @@ public class StatusController : ControllerBase
         return Ok(new { status = "Running" });
     }
 
+    [HttpGet("team")]
+    public IActionResult GetTeamStatus()
+    {
+        return Ok(new { team = "Equipo 1", status = "Ready" });
+    }
 }
