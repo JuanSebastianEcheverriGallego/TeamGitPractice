fe0dc35921e8fc3bb8bdc1f4ccfcd2279df49907
Marbel Juliana Mejia Bedoya <marbeljulianamejiabedoya2003@gmail.com>
2026-09-03 19:52:00 -0500

    feat: añadir endpoint api/version

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetVersion()
+        {
+            return Ok(new { version = "1.0.0" });
+        }
+    }
+}
