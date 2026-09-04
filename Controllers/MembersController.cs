703fd01bae36b33302c43b5af18d84884b2cf02a
David Stiven Franco Lopez <davidfranco259514@correo.itm.edu.co>
2026-09-03 22:16:00 -0500

    feat: add assigned API endpoint

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetMembers()
+    {
+        var members = new[]
+        {
+            "Juan Sebastián Echeverri Gallego",
+            "David Stiven Franco Lopez",
+            "Marbel Juliana Mejía Bedoya",
+            "Jhon Fernando Sánchez Álvarez",
+            "Marlon García Sepúlveda"
+        };
+        return Ok(members);
+    }
+
+}
